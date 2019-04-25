11ecbe2284ab17f9044ab6f5065a76b5a0be88f6
Kyle Parisi <kyleparisi@gmail.com>
2019-04-25 18:50:53 -0400

    Relayout app for panning

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,50 @@
 <template>
-  <div id="app" class="h-100 bg-black-passive" :style="panning">
-    <div class="absolute top-0 left-0" :style="app.style">
-      <svg :style="app.svgStyle" v-on:dblclick="configuring = {}">
-        <marker
-                id="circle"
-                markerWidth="4"
-                markerHeight="4"
-                refX="2"
-                refY="2"
-        >
-          <circle cx="2" cy="2" r="2" stroke="none" fill="#c3fdff"></circle>
-        </marker>
-        <Link :link="link"
-              :sourceX="nodes[link.source.node].position.right"
-              :sourceY="nodes[link.source.node].position.top"
-              :targetX="nodes[link.target.node].position.left"
-              :targetY="nodes[link.target.node].position.top"
-              v-for="link in links"></Link>
-        <Packet :link="link" v-for="link in links" v-if="packets[link.id]"></Packet>
-      </svg>
+  <div>
+    <div id="app" class="h-100 bg-black-passive" :style="panning">
+      <div class="absolute top-0 left-0" :style="app.style">
+        <svg :style="app.svgStyle" v-on:dblclick="configuring = {}">
+          <marker
+                  id="circle"
+                  markerWidth="4"
+                  markerHeight="4"
+                  refX="2"
+                  refY="2"
+          >
+            <circle cx="2" cy="2" r="2" stroke="none" fill="#c3fdff"></circle>
+          </marker>
+          <Link :link="link"
+                :sourceX="nodes[link.source.node].position.right"
+                :sourceY="nodes[link.source.node].position.top"
+                :targetX="nodes[link.target.node].position.left"
+                :targetY="nodes[link.target.node].position.top"
+                v-for="link in links"></Link>
+          <Packet :link="link" v-for="link in links" v-if="packets[link.id]"></Packet>
+        </svg>
 
-      <Node :node="node" v-for="node in nodes"></Node>
+        <Node :node="node" v-for="node in nodes"></Node>
 
-      <WebViewNode v-if="webview && webview.src"></WebViewNode>
+        <WebViewNode v-if="webview && webview.src"></WebViewNode>
 
+      </div>
     </div>
+    <div>
+      <Configuration v-if="configuring.module"></Configuration>
 
-    <Configuration v-if="configuring.module"></Configuration>
+      <div class="fixed left-0 bottom-0 fw6 w-100" v-if="configuring.module">
+        <codemirror :style="{height: '400px'}" v-model="nodes[configuring.id].run" v-if="nodes[configuring.id].run !== undefined" :options="{theme: 'dracula', mode: 'javascript'}"></codemirror>
+        <codemirror :style="{height: '400px'}" v-model="nodes[configuring.id].template" v-if="nodes[configuring.id].template !== undefined" :options="{theme: 'dracula', mode: 'pug'}"></codemirror>
+      </div>
 
-    <div class="fixed left-0 bottom-0 fw6 w-100" v-if="configuring.module">
-      <codemirror :style="{height: '400px'}" v-model="nodes[configuring.id].run" v-if="nodes[configuring.id].run !== undefined" :options="{theme: 'dracula', mode: 'javascript'}"></codemirror>
-      <codemirror :style="{height: '400px'}" v-model="nodes[configuring.id].template" v-if="nodes[configuring.id].template !== undefined" :options="{theme: 'dracula', mode: 'pug'}"></codemirror>
-    </div>
+      <SearchNodes></SearchNodes>
 
-    <SearchNodes></SearchNodes>
-
-    <div class="fixed right-1 bottom-1 gray z-2" v-if="Object.keys(selectedLinks).length">
-      <div class="flex items-center">
-        <div class="pointer orange" @click="addLoggingToLinks">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mic"><path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"></path><path d="M19 10v2a7 7 0 0 1-14 0v-2"></path><line x1="12" y1="19" x2="12" y2="23"></line><line x1="8" y1="23" x2="16" y2="23"></line></svg>
-        </div>
-        <div class="pointer" @click="addLoggingToLinks">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mic-off"><line x1="1" y1="1" x2="23" y2="23"></line><path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6"></path><path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23"></path><line x1="12" y1="19" x2="12" y2="23"></line><line x1="8" y1="23" x2="16" y2="23"></line></svg>
+      <div class="fixed right-1 bottom-1 gray z-2" v-if="Object.keys(selectedLinks).length">
+        <div class="flex items-center">
+          <div class="pointer orange" @click="addLoggingToLinks">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mic"><path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"></path><path d="M19 10v2a7 7 0 0 1-14 0v-2"></path><line x1="12" y1="19" x2="12" y2="23"></line><line x1="8" y1="23" x2="16" y2="23"></line></svg>
+          </div>
+          <div class="pointer" @click="addLoggingToLinks">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mic-off"><line x1="1" y1="1" x2="23" y2="23"></line><path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6"></path><path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23"></path><line x1="12" y1="19" x2="12" y2="23"></line><line x1="8" y1="23" x2="16" y2="23"></line></svg>
+          </div>
         </div>
       </div>
     </div>
